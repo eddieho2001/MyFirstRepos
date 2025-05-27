cedb42c514a04645e9dfcd2ae849531451d98a9d
eddieho <eddie.ho@oneport.com>
2025-05-27 17:19:00 +0800

    complete created cmake build

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -34,6 +34,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("D:/MyDocs/GameDev/Udemy/LearnC++AndMakeaGameFromScratch/Section06/LightYearsGameProj/build/_deps/sfml-build/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("D:/MyDocs/GameDev/Udemy/LearnC++AndMakeaGameFromScratch/Section06/LightYearsGameProj/build/game/cmake_install.cmake")
 endif()
 
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
+#include<SFML/Graphics.hpp>
 
 int main(){
-	std::cout<<"Testing...3"<<std::endl;
+
+	sf::RenderWindow renderWin{ sf::VideoMode({500, 700}), "Test Win"};
+	while (renderWin.isOpen()) {
+		while (const std::optional evt = renderWin.pollEvent()) {
+			if (evt->is<sf::Event::Closed>()) {
+				renderWin.close();
+			}
+		}
+
+		//renderWin.clear();
+
+		//renderWin.draw();
+		
+	}
+	
+	//std::cout<<"Testing...3"<<std::endl;
 	
 	return 0;
 }
